10bca31b95b581c2c38be6d52416bb43a34591aa
Atanu Mondal <atanumandal45@gmail.com>
2021-01-18 19:02:41 +0530

    added the context user name and conditional navBar

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,30 +1,62 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext } from "react";
 
-import {Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink, NavbarBrand, NavbarText} from 'reactstrap';
-import {Link} from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarBrand,
+  NavbarText,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
-const Header =()=> {
-    return (
-        <Navbar color="info" light expand='md'>
-            <NavbarBrand className='font-weight-bold'>
-                <Link to='/' className='text-white' >Git User Search</Link>
-            </NavbarBrand>
-            <NavbarToggler/>
-            <Collapse navbar>
-                <Nav className='ml-auto'>
-                    <NavItem>
-                        <NavLink  className='text-white'>Sign In</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='text-white'>Sign Up</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink  className='text-white'>Log Out</NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
-    )
-}
+const Header = () => {
+  const context = useContext(UserContext);
 
-export default Header
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="info" light expand="md">
+      <NavbarBrand className="font-weight-bold">
+        <Link to="/" className="text-white">
+          Git User Search
+        </Link>
+      </NavbarBrand>
+      <NavbarText className="text-white">
+        {context.user?.email ? context.user.email : ""}
+      </NavbarText>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          {context.user ? (
+            <NavItem>
+              <NavLink tag={Link} to="/" className="text-white">
+                Log Out
+              </NavLink>
+            </NavItem>
+          ) : (
+            <>
+              <NavItem>
+                <NavLink tag={Link} to="/signin" className="text-white">
+                  Sign In
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/signup" className="text-white">
+                  Sign Up
+                </NavLink>
+              </NavItem>
+            </>
+          )}
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
