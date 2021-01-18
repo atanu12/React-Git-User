b20cc270d74687548c1c05313ff2eb55c6257916
Atanu Mondal <atanumandal45@gmail.com>
2021-01-18 17:37:23 +0530

    install all the dependency and created the pages for route through all the pages

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound =()=> {
+    return (
+        <div>
+            <h1>page Not Found 404 Error</h1>
+        </div>
+    )
+}
+
+export default PageNotFound
