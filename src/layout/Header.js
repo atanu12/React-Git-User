a8bcd862d958f37ceb07fa94e3fb58b6fc34ff31
Atanu Mondal <atanumandal45@gmail.com>
2021-01-25 12:20:15 +0530

    created the sign-in and sign-up function in the home page

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
         <Nav className="ml-auto" navbar>
           {context.user ? (
             <NavItem>
-              <NavLink tag={Link} to="/" className="text-white">
+              <NavLink onClick={ ()=> {context.setUser(null)} } className="text-white">
                 Log Out
               </NavLink>
             </NavItem>
